43be039b69a79818fa871ab087ef748ac49312f3
anton <antonchervyakov2003@bk.ru>
2025-11-20 14:06:19 +0300

    fix bag

--- ui/components/Audio/Audio.component.js
+++ ui/components/Audio/Audio.component.js
@@ -4,12 +4,15 @@ import { subcribe } from "../../../core/state-manager.js"
 export function AudioComponent(){
     const catchAudio = new Audio('../sounds/341695__projectsu012__coins-1.mp3')
     const missAudio = new Audio('../sounds/heartbeatloop.mp3')
-
+    
+console.log(catchAudio)
     subcribe((e) =>{
         if(e.name === EVENTS.GOOGLE_RAN_AWAY){
+            missAudio.crossOrigin="anonymous"
             missAudio.play()
         }
         if(e.name === EVENTS.GOOGLE_CAUGHT){
+          catchAudio.crossOrigin="anonymous"
           catchAudio.currentTime = 0;  
           catchAudio.play()
         }
